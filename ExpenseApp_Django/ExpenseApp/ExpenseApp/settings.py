37397d4ce595df8e4dc2384313a3f9385982adb1
kachmark4423 <70001753+kachmark4423@users.noreply.github.com>
2024-09-10 11:20:44 -0500

    Update settings.py

--- ExpenseApp_Django/ExpenseApp/ExpenseApp/settings.py
+++ ExpenseApp_Django/ExpenseApp/ExpenseApp/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+#*uemo=+0@%olf66fn6^%dq)@j&q1x)swi+i8*-&+cg=)k(u$'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
